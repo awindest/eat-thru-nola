c3536774560fd87c62373b129286cdf5dbabb265
Bill Indest <bindest@macos-c02g6363md6n.interos.ai>
2025-06-07 12:25:48 -0400

    Added turtle soup and crawfish etouffee recipes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,9 @@
 <script>
+	import '../app.css'
+
 	let { children } = $props();
+	
 </script>
 
 {@render children?.()}
 
-<style>
-    @font-face {
-        font-family: 'TamaraElainaDemoRegular';
-        src: url('/fonts/TamaraElainaDemoRegular.ttf');
-        font-style: normal;
-        font-weight: 500;
-        font-display: swap;
-    }
-    :global(body) {
-        font-family: 'TamaraElainaDemoRegular', san-serif;
-        line-height: 1.5; 
-    }
-</style>
